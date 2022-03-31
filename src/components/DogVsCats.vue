72fdf8c4de8a25ad8a4160df35942f10ee737665
Hisao <jaimehisao2011@gmail.com>
2022-03-31 00:22:32 -0600

    dog vs cats hider

---
+++ src/components/DogVsCats.vue
@@ -0,0 +1,25 @@
+<template>
+  <html>
+  <head></head>
+  <body>
+  <div>
+    <a v-if="disappear == 0">Los perros son mejores que los gatos!</a>
+  </div>
+  <button @click="disappear = (disappear + 1) % 2;">Los gatos son mejores!</button>
+  </body>
+  </html>
+</template>
+
+<script>
+export default {
+  data() {
+    let disappear = 0;
+    return {
+      disappear: disappear
+    };
+  }
+};
+</script>
+
+<style>
+</style>
